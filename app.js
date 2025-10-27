279a25eb34d0429e6720d6a7eef2f6d3e35c6729
mpolitti <marcopolitti.br@gmail.com>
2025-10-27 07:15:18 -0300

    Recuperando os valores digitados pelo usuário via função sortear.

---
+++ app.js
@@ -0,0 +1,8 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+    //console.log(`Quantidade: ${quantidade}`);
+    //console.log(`De: ${de}`);
+    //console.log(`Ate: ${ate}`);
+}
